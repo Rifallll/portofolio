5760f1887a9b166baba6215ef0fbeccaa90aed5e
RIFAL <permanarifal269@gmail.com>
2025-09-23 19:44:59 +0700

    [dyad] Memperbaiki kesalahan React.Children.only di Navbar - wrote 1 file(s)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,12 +38,8 @@ const Navbar = () => {
         {isMobile ? (
           <Sheet>
             <SheetTrigger asChild>
-              <Button variant="outline" size="icon">
-                {/* Membungkus anak-anak tombol dalam satu elemen span */}
-                <span>
-                  <Menu className="h-6 w-6" />
-                  <span className="sr-only">Toggle navigation menu</span>
-                </span>
+              <Button variant="outline" size="icon" aria-label="Toggle navigation menu">
+                <Menu className="h-6 w-6" />
               </Button>
             </SheetTrigger>
             <SheetContent side="right">
