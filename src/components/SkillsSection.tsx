45ef18144355ce91be792e75f3da89d269da89f6
RIFAL <permanarifal269@gmail.com>
2025-09-23 18:32:24 +0700

    [dyad] Menghapus tombol "View My Projects" dari bagian keahlian - wrote 1 file(s)

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -26,13 +26,7 @@ const SkillsSection = () => {
             <SkillItem key={index} {...skill} />
           ))}
         </div>
-        <Button
-          onClick={() => scrollToSection("projects")}
-          className="bg-gray-900 text-white hover:bg-gray-700 flex items-center space-x-2 px-8 py-4 text-lg mx-auto"
-        >
-          <span>View My Projects</span>
-          <ArrowRight className="h-5 w-5" />
-        </Button>
+        {/* "View My Projects" button removed as requested */}
       </div>
     </section>
   );
