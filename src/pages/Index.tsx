20dbe8b19ea01365acac43f1d2983d0a9bb82814
RIFAL AZHAR PERMANA <125524926+Rifallll@users.noreply.github.com>
2025-11-18 18:49:48 +0700

    [dyad] Menambahkan bagian fitur baru - wrote 2 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,13 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-// Removed other portfolio sections as they are not part of the new design
+import FeaturesSection from "@/components/FeaturesSection"; // Import the new component
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Navbar />
       <HeroSection />
-      {/* Other sections like SkillsSection, ProjectsSection, AboutSection, CertificatesSection, ContactSection are removed for this new design */}
+      <FeaturesSection /> {/* Add the FeaturesSection here */}
     </div>
   );
 };
