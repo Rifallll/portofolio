cd98d8186080f29ad33fa090dbb1eac3054511ca
RIFAL AZHAR PERMANA <125524926+Rifallll@users.noreply.github.com>
2026-02-06 16:12:53 +0700

    Updates

--- assets/TestimonialsSection-CR5KBOWS.js
+++ assets/TestimonialsSection-CR5KBOWS.js
@@ -1,4 +1,4 @@
-import{c as n,j as e,m as i,S as l,a as r,b as d}from"./index-BKz0lQTY.js";/**
+import{c as n,j as e,m as i,S as l,a as r,b as d}from"./index-Bv5rEL1k.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
